11b1be63f71fedcc8313ca1164a3203945565e75
Arjunan Ravi <arjunanatwork@gmail.com>
2019-10-24 20:10:22 +0400

    Added CartIcon and CartDropdown Component to our application and also implemented cartReducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom';
 
 import { auth } from "../../firebase/firebase.utils";
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo'/>
@@ -20,15 +22,21 @@ const Header = ({currentUser}) => (
                 CONTACT
             </Link>
             {
-                currentUser ? <div className='option' onClick={()=> auth.signOut()}>SIGN OUT</div>
+                currentUser ?
+                    <div className='option' onClick={()=> auth.signOut()}>SIGN OUT</div>
                     : <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon/>
         </div>
+        {
+            hidden ? null : <CartDropdown/>
+        }
     </div>
 )
 
-const mapStatetoProps = (state) => ({
-    currentUser: state.user.currentUser
+const mapStatetoProps = ({user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStatetoProps)(Header); // HOC
