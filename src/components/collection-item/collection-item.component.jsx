deec727420e416d2fe21d821e62c7583ef546b55
Arjunan Ravi <arjunanatwork@gmail.com>
2019-10-24 21:11:19 +0400

    Updated Shop Data with New Data, Updated Cart Utils to Group Items in Cart Reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import { connect } from "react-redux";
 
+import { addItem } from "../../redux/cart/cart.actions";
+import CustomButton from "../custom-button/custom-button.component";
 import './collection-item.styles.scss';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
-    <div className='collection-item'>
-        <div className='image' style={{backgroundImage: `url(${imageUrl})`}}/>
-        <div className='collection-footer'>
-            <div className="name">{name}</div>
-            <div className="price">{price}</div>
-        </div>
-    </div>
-)
+const CollectionItem = ({item, addItem}) => {
+    const {name, price, imageUrl} = item
 
-export default CollectionItem;
+    return (
+        <div className='collection-item'>
+            <div className='image' style={{backgroundImage: `url(${imageUrl})`}}/>
+            <div className='collection-footer'>
+                <div className="name">{name}</div>
+                <div className="price">{price}</div>
+            </div>
+            <CustomButton onClick={()=> addItem(item)} inverted> Add to Cart </CustomButton>
+        </div>
+    )
+}
+
+const mapDispatchtoProps = dispatch => ({
+    // prop that gets passed in to component
+    addItem: item => dispatch(addItem(item)) // Calls the addItem function in action which returns a object with the payload and dispatches into the store
+})
+export default connect(null, mapDispatchtoProps)(CollectionItem);
