235bc9a3eca7ac368b0ac06ee3b54247788ce161
Arjunan Ravi <arjunanatwork@gmail.com>
2019-11-15 14:17:26 +0400

    Adding Redux Thunk to Project

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,46 +1,41 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { createStructuredSelector} from "reselect";
 import CollectionOverview from "../../components/collections-overview/collections-overview.component"
 import CollectionPage from "../collection/collection.component";
-import { firestore, convertCollectionSnapshotToMap} from "../../firebase/firebase.utils";
-import { updateCollections } from "../../redux/shop/shop.action";
+import { fetchCollectionsStartAsync } from "../../redux/shop/shop.action";
+import {selectIsCollectionFetching, selectIsCollectionsLoaded} from "../../redux/shop/shop.selector";
 import WithSpinner from "../../components/withSpinner/with-spinner.component";
+
 
 const CollectionsOverviewWithSpinner = WithSpinner(CollectionOverview);
 const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
 
-    state = {
-        loading: true
-    }
-
-    unSubscribeFromSnapshot = null;
-
     componentDidMount() {
-        const { updateCollections } = this.props;
-        const collectionRef = firestore.collection('collections');
-        this.unSubscribeFromSnapshot =  collectionRef.onSnapshot(async snapshot => {
-            const collectionMap =  convertCollectionSnapshotToMap(snapshot);
-            updateCollections(collectionMap);
-            this.setState({loading: false});
-        })
+        const { fetchCollectionsStartAsync } = this.props;
+        fetchCollectionsStartAsync();
     }
 
     render() {
-        const { match } = this.props;
-        const { loading} = this.state;
+        const { match, isFetchingCollections, isCollectionsLoaded } = this.props;
         return (
             <div className='shop-page'>
-                <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />}/>
-                <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} />}/>
+                <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={isFetchingCollections} {...props} />}/>
+                <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={!isCollectionsLoaded} {...props} />}/>
             </div>
         )
     }
 }
 
+const mapStateToProps = createStructuredSelector({
+    isFetchingCollections: selectIsCollectionFetching,
+    isCollectionsLoaded: selectIsCollectionsLoaded
+});
+
 const mapDispatchToProps = dispatch => ({
-    updateCollections: collectionMap => dispatch(updateCollections(collectionMap))
+    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
 })
-export default connect(null, mapDispatchToProps)(ShopPage);
+export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
--- src/redux/shop/shop.action.js
+++ src/redux/shop/shop.action.js
@@ -1,6 +1,35 @@
 import ShopActionTypes from "./shop.types";
+import {convertCollectionSnapshotToMap, firestore} from "../../firebase/firebase.utils";
 
 export const updateCollections = (collectionMap) => ({
     type: ShopActionTypes.UPDATE_COLLECTIONS,
     payload: collectionMap
 })
+
+export const fetchCollectionsStart = () => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_START
+})
+
+export const fetchCollectionsSuccess = (collectionMap) => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+    payload: collectionMap
+})
+
+export const fetchCollectionsFailure = (errorMessage) => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+    payload: errorMessage
+})
+
+// We write a function that returns a functions that gets dispatch in it
+//If redux-thunk middleware is enabled, any time you attempt to dispatch a function instead of an object, the
+//middleware will call that function with dispatch method itself as the first argument
+export const fetchCollectionsStartAsync = () => {
+    return dispatch => {
+        const collectionRef = firestore.collection('collections');
+        dispatch(fetchCollectionsStart());
+        collectionRef.get().then(snapshot => {
+            const collectionMap =  convertCollectionSnapshotToMap(snapshot);
+            dispatch(fetchCollectionsSuccess(collectionMap))
+        }).catch(error => dispatch(fetchCollectionsFailure(error.message)))
+    }
+}
